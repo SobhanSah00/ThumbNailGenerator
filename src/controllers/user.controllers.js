67947da20851cfeb346621cb5c3366ab2d6b7bab
sobhan9012 <sobhansahoo2000@gmail.com>
2024-09-18 12:41:47 +0530

    "Updated package-lock.json, package.json, user.controllers.js, and removed user.validation.js"

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,13 +1,12 @@
-import { asyncHandeler } from "../utils/asyncHandeler.js"
-import { ApiResponse } from "../utils/ApiResponse.js"
-import { ApiError } from "../utils/ApiError.js"
-import { User } from "../models/User/user.models.js"
-import userSchema from "../validation/user.validation.js"
+import { asyncHandeler } from "../utils/asyncHandeler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/User/user.models.js";
 
-const registerUser = asyncHandeler(async (req,res) => {
-    b
-})
+const registerUser = asyncHandeler(async (req, res) => {
+    const {username,fullname,email,password} = req.body;
+    console.log(username,fullname,email,password);
+    res.json(200,new ApiResponse(200,"user information is extracted successfully . "))
+});
 
-export {
-    registerUser
-}
+export { registerUser };
